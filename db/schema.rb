729a719131ce72a407d35d4bc67b21d72e948f97
Hamilton Clark <dhclark2@ncsu.edu>
2016-12-16 14:06:29 -0500

    Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161216175006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "seller"
+    t.string   "address"
+    t.text     "description"
+    t.integer  "price"
+    t.integer  "sq_ft"
+    t.integer  "bedrooms"
+    t.integer  "bathrooms"
+    t.string   "agent"
+    t.string   "picture_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
