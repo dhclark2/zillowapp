76648a761acf0b89fa9e55c39bda609cab064f89
Hamilton Clark <dhclark2@ncsu.edu>
2016-12-20 15:31:41 -0500

    added locking the pages besides home page unless logged in.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-  # before_action :authenticate_user!, except:[:index]
+  before_action :authenticate_user!, except:[:index]
 
   # GET /listings
   def index
